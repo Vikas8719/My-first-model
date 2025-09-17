d36f8a5ba0b3b79877d670b482063b7d162eb637
Vikas8719 <vikasawasthi717@gmail.com>
2025-09-17 09:49:05 +0530

    titanic project

---
+++ firstproject_titanic.py
@@ -0,0 +1,18 @@
+{
+  "cells": [],
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOomMmKpFunyLB2kIrU66XJ"
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
